24631c43904e90bd3563d48c4ff53027c3df8239
coderadu <47945947+coderadu@users.noreply.github.com>
2022-04-28 19:44:17 +0300

    Added rest of functions

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,13 +1,15 @@
 import esbuild from 'esbuild'
+import { readdirSync, copyFileSync, existsSync, rmSync } from 'fs'
 
 export default function (args: string[], success: Function, error: Function) {
+  if (existsSync('./dist')) rmSync('./dist', { recursive: true })
   console.log('Building for production...')
   esbuild
     .build({
       entryPoints: ['./src/index.tsx'],
       bundle: true,
-      minify: true,
       allowOverwrite: true,
+      minify: true,
       outfile: './dist/index.js',
       platform: 'browser',
     })
@@ -15,6 +17,13 @@ export default function (args: string[], success: Function, error: Function) {
       error(e)
     })
     .then(() => {
+      if (existsSync('./public')) {
+        console.log('Copying static files...')
+        const publicFiles = readdirSync('./public')
+        publicFiles.forEach((file) =>
+          copyFileSync(`./public/${file}`, `./dist/${file}`)
+        )
+      } else console.log('No static files found.')
       success()
     })
 }
