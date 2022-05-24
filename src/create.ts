c9e1ee39f14bcec32bf64e4d561b74e363fdd912
Radu <radu.gabi.m@gmail.com>
2022-05-24 11:51:53 +0000

    added templates

--- src/create.ts
+++ src/create.ts
@@ -2,14 +2,24 @@
 import { readdirSync } from 'fs'
 import { copySync } from 'fs-extra'
 
+let template='default'
+
 if (readdirSync('.').length !== 0) {
   console.error('Error: CWD is not empty')
   process.exit(1)
 }
 
-console.log('Creating ESbuild React app...')
-readdirSync(`${__dirname}/../skel`).forEach((file) =>
-  copySync(`${__dirname}/../skel/${file}`, file)
+const args = process.argv.slice(2)
+
+for(const arg of args) {
+  if(arg=="--template" || arg=="-t"){
+    template = args[args.indexOf(arg)+1]
+  }
+}
+
+console.log(`Creating ESbuild React app with ${template} template...`)
+readdirSync(`${__dirname}/../skel/${template}`).forEach((file) =>
+  copySync(`${__dirname}/../skel/${template}/${file}`, file)
 )
 console.log(
   'Done. Please run `npm install` and `npm run dev` to start the development server.'
